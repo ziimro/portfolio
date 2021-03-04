0a45f1f7bc78f6b93c6eb8016eef7a713c6e6fc9
ziimro <chwaliszjonasz@gmail.com>
2021-03-04 11:20:04 +0100

    organizeing files

--- jquerytojs.js
+++ jquerytojs.js
@@ -1,33 +1,33 @@
 let ready = (callback) => {
-    if (document.readyState != "loading") callback();
-    else document.addEventListener("DOMContentLoaded", callback);
+    if (document.readyState != "loading") callback()
+    else document.addEventListener("DOMContentLoaded", callback)
   }
   
 ready(() => { 
-    let baseUrl = "/galleryimages/";
-    let pictureIndex = 0;
-    let pictures = [];
+    let baseUrl = "/galleryimages/"
 
     // loading and listing files
     function getFiles() {
-        fetch(baseUrl) 
-            .then((data) => {
-            pictures = [];
-            let elements = document.querySelectorAll("a[href]");
-            Array.prototype.forEach.call(elements, function(el, i) {
-                let href = el.getAttribute('href');
-                let imagebox;
-                if (href.indexOf('.webp') > 0) {
-                    console.log("if statement finished")
-                    imagebox = document.createElement("div");
-                    imagebox.className = "imagebox";
-                    let img = document.createElement("img");
-                    img.src = href;
-                    imagebox.append(img)
-                    document.getElementById("#viewer").append(imagebox);
-                }
-            });
-        });
+        var request = new XMLHttpRequest();
+        request.open('GET', baseUrl, true);
+
+        request.onload = function(data) {
+            if (this.status >= 200 && this.status < 400) {
+                let datainfo = document.querySelectorAll("a[href]");
+                
+                var elements = document.querySelectorAll("a[href]");
+                Array.prototype.forEach.call(elements, function(data){
+                    var href = this.getAttribute('href');
+                    var imagebox;
+                    if (href.indexOf('.webp') > -1) {
+                        console.log("sucsess!!")
+                    }
+                });
+            } else {
+                // We reached our target server, but it returned an error
+            }
+        };
+        request.send();
     }
     getFiles();
 });
